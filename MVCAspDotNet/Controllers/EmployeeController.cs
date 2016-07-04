58485a76eed5b624da65463840edf51ea58b185a
rajdharak <raj@promact.com>
2016-07-04 15:04:35 +0530

    project with Model

---
+++ MVCAspDotNet/Controllers/EmployeeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCAspDotNet.Models;
+
+namespace MVCAspDotNet.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Details()
+        {
+            Employee emp = new Employee()
+            {
+                EmployeeID=101,
+                Name= "Dharak",
+                City="Vadodara"
+            };
+            return View(emp);
+        }
+    }
+}
